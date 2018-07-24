4fc9e572de30279fce7b222e2dd3fbece28cf47b
Jordan Alexander <happyperson@Jordans-MacBook-Pro-8.local>
2018-07-23 23:07:30 -0700

    Adding broken vases env

--- envs/vases_spec.py
+++ envs/vases_spec.py
@@ -26,6 +26,7 @@ class VasesEnvSpec2x3(object):
         self.table_mask = np.array([[0, 0, 1],
                                     [0, 0, 0]], dtype='bool')
 
+
 class VasesEnvState2x3(object):
     '''
     state of the environment; describes positions of all objects in the env.
@@ -37,6 +38,61 @@ class VasesEnvState2x3(object):
                                 [0, 0, 0]], dtype='bool')
         self.bv_pos = np.array([[0, 0, 0],
                                 [0, 0, 0]], dtype='bool')
+        self.a_pos = np.array([[[0, 1, 0],
+                              [0, 0, 0]],
+                             [[0, 0, 0],
+                              [0, 0, 0]],
+                             [[0, 0, 0],
+                              [0, 0, 0]],
+                             [[0, 0, 0],
+                              [0, 0, 0]]], dtype='bool')
+
+        self.t_pos = np.array([[1, 0, 0],
+                                [0, 0, 0]], dtype='bool')
+        # Variable determining whether the agent is carrying something:
+        # [0, 0] -> nothing, [1, 0] -> vase, [0, 1] -> tablecloth
+        self.carrying = np.array([0, 0], dtype='bool')
+
+
+class VasesEnvSpec2x3Broken(object):
+    def __init__(self):
+
+        self.d_mask = np.array([[1, 0, 1],
+                                [0, 0, 0]], dtype='bool')
+        self.v_mask = np.array([[1, 0, 1],
+                                [0, 0, 0]], dtype='bool')
+        self.bv_mask = np.array([[0, 1, 0],
+                                 [1, 1, 1]], dtype='bool')
+        self.agent_mask = np.array([[[0, 1, 0],
+                                    [1, 1, 1]],
+                                   [[0, 1, 0],
+                                    [1, 1, 1]],
+                                   [[0, 1, 0],
+                                    [1, 1, 1]],
+                                   [[0, 1, 0],
+                                    [1, 1, 1]]], dtype='bool')
+        self.t_mask = np.array([[1, 1, 1],
+                                [1, 1, 1]], dtype='bool')
+
+        self.n_v = 3
+        self.n_t = 1
+
+        self.table_mask = np.array([[0, 0, 1],
+                                    [0, 0, 0]], dtype='bool')
+
+
+class VasesEnvState2x3Broken(object):
+    '''
+    state of the environment; describes positions of all objects in the env.
+    initial state contains broken vases
+    '''
+    def __init__(self):
+        self.d_pos = np.array([[1, 0, 1],
+                                [0, 0, 0]], dtype='bool')
+        self.v_pos = np.array([[1, 0, 0],
+                                [0, 0, 0]], dtype='bool')
+        self.bv_pos = np.array([[0, 0, 0],
+                                [1, 0, 1]], dtype='bool')
         self.a_pos = np.array([[[0, 1, 0],
                               [0, 0, 0]],
                              [[0, 0, 0],
