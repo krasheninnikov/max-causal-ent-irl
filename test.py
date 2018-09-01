61fca1739fd269f39e68beeb45242cade764f84e
Rohin Shah <rohinmshah@gmail.com>
2018-09-01 12:27:55 -0700

    Add argument parsing structure to test.py

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import numpy as np
 import sys
 
+import argparse
 from envs.vases_grid import VasesGrid, VasesEnvState, print_state, str_to_state, state_to_str
 from envs.vases_spec import VasesEnvState2x3V2D3, VasesEnvSpec2x3V2D3, VasesEnvState2x3Broken, VasesEnvSpec2x3Broken
 
@@ -32,7 +33,7 @@ def forward_rl(env, r, h=40, temp=.1, steps_printed=15, current_s=None):
         print(obs, obs.T @ env.r_vec)
         print()
 
-def experiment_wrapper(environment,
+def experiment_wrapper(env,
                      algorithm,
                      rl_algorithm,
                      horizon=22, #number of steps we assume the expert was acting previously
@@ -41,11 +42,6 @@ def experiment_wrapper(environment,
                      epochs = 200,
                      s_current=None,
                      uniform=False):
-
-    if environment == "vases":
-      env = VasesGrid(VasesEnvSpec2x3V2D3(), VasesEnvState2x3V2D3())
-    elif environment == "boxes":
-      env = BoxesEnv(BoxesEnvSpec6x6(), BoxesEnvState6x6())
 
     print('Initial state:')
     print_state(env.init_state)
@@ -63,6 +59,8 @@ def experiment_wrapper(environment,
         with printoptions(precision=4, suppress=True):
             print(); print('Final reward vector: ', r_vec)
         return r_vec
+    else:
+        raise ValueError('Unknown algorithm: {}'.format(algorithm))
 
     if rl_algorithm == "vi":
         forward_rl(env, r_rl + r_vec)
@@ -73,5 +71,53 @@ def experiment_wrapper(environment,
             env.print_state(env.s, env.spec)
             env.step(np.random.randint(0, env.nA))
 
-#print(experiment_wrapper("vases", "om", "rl"))
-print(experiment_wrapper("boxes", "", "test"))
+
+
+def parse_args(args=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-e', '--env', type=str, default='vases',
+                        help='Environment to run: one of [vases, boxes]')
+    parser.add_argument('-a', '--algorithm', type=str, default='om',
+                        help='Frame condition inference algorithm. Only om for now.')
+    parser.add_argument('-r', '--rl_algorithm', type=str, default='vi',
+                        help='Algorithm to run on the inferred reward')
+    parser.add_argument('-H', '--horizon', type=int, default=22,
+                        help='Number of timesteps we assume the human has been acting')
+    parser.add_argument('-t', '--temperature', type=float, default=1.0,
+                        help='Boltzmann rationality constant for the human')
+    parser.add_argument('-l', '--learning_rate', type=float, default=0.1,
+                        help='Learning rate for gradient descent')
+    parser.add_argument('-p', '--epochs', type=int, default=200,
+                        help='Number of epochs to run gradient descent for.')
+    parser.add_argument('-s', '--state', type=str,
+                        help='Specifies the observed state.')
+    parser.add_argument('-u', '--uniform_prior', action='store_true',
+                        help='If specified, use the uniform prior over initial states. Default is to know the initial state.')
+
+    parser.add_argument('-o', '--output', type=str, help='Output file name')
+    return parser.parse_args(args)
+
+
+def main():
+    args = parse_args()
+
+    if args.env == "vases":
+        env = VasesGrid(VasesEnvSpec2x3V2D3(), VasesEnvState2x3V2D3())
+    elif args.env == "boxes":
+        env = BoxesEnv(BoxesEnvSpec6x6(), BoxesEnvState6x6())
+    else:
+        raise ValueError('Unknown environment: {}'.format(args.env))
+
+    results = experiment_wrapper(env,
+                                 args.algorithm,
+                                 args.rl_algorithm,
+                                 args.horizon,
+                                 temp = args.temperature,
+                                 learning_rate = args.learning_rate,
+                                 epochs = args.epochs,
+                                 s_current = None,
+                                 uniform = args.uniform_prior)
+    print(results)
+
+if __name__ == '__main__':
+    main()
