8d10977e3b6b2988dbc62b303a6bdb3bd026799b
Jordan Alexander <happyperson@DN0a242122.SUNet>
2018-09-24 15:30:39 -0700

    Relative reachability update

--- relative_reachability.py
+++ relative_reachability.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def relative_reachability_penalty(mdp, horizon, start, weight=1):
+def relative_reachability_penalty(mdp, horizon, start):
     '''
     Calculates the undiscounted relative reachability penalty for each state in an mdp, compared to the starting state baseline.
 
@@ -12,19 +12,25 @@ def relative_reachability_penalty(mdp, horizon, start, weight=1):
         coverage = np.maximum.reduce([coverage[mdp.deterministic_T[:, a], :] for a in range(mdp.nA)])
     
     r_r = np.sum(np.maximum(coverage[mdp.get_num_from_state(start), :] - coverage, 0), axis=1)
-    return  r_r / np.amax(r_r)
+    return  r_r / np.amax(r_r) 
 
- def stochastic_relative_reachability_penalty(mdp, horizon, start, weight=1):
+def stochastic_relative_reachability_penalty(mdp, horizon, start):
     '''
     Calculates the undiscounted relative reachability penalty for each state in an mdp, compared to the starting state baseline. 
      
     Based on the algorithm described in: https://arxiv.org/pdf/1806.01186.pdf
     '''
-    mdp.get_transition_matrix()
+    # mdp.get_transition_matrix()
+    mdp.T = np.zeros((mdp.nS, mdp.nA, mdp.nS))
+    for s in range(mdp.nS):
+        for a in range(mdp.nA):
+            mdp.T[s, a, mdp.deterministic_T[s, a]] = 1
+
     transition = np.reshape(mdp.T, (mdp.T.shape[1], mdp.T.shape[0], mdp.T.shape[2]))
-    coverage = np.ones((mdp.nS, mdp.nS))
+    coverage = np.identity(mdp.nS)
     for i in range(horizon):
         coverage = np.amax(transition * coverage, axis=0)
     
-    r_r = np.sum(np.maximum(coverage - coverage[start, :], 0), axis=1)
+    r_r = np.sum(np.maximum(coverage[mdp.get_num_from_state(start), :] - coverage, 0), axis=1)
+    #TODO: Consider nans
     return r_r / np.amax(r_r)
--- test.py
+++ test.py
@@ -36,7 +36,8 @@ def forward_rl(env, r_planning, r_true, h=40, temp=.1, last_steps_printed=3,
         diff = env.f_matrix - env.s_to_f(env.get_state_from_num(current_s)).T
         r_s -= weight * np.linalg.norm(diff, axis=1)
     if relative_reachability:
-        r_r = relative_reachability_penalty(env, h, env.s, weight=env.nF)
+        r_r = stochastic_relative_reachability_penalty(env, h, env.s)
+        print(r_r)
         r_s -= weight * r_r
 
     # For evaluation, plan optimally instead of Boltzmann-rationally
