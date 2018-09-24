956577dad4d33837a87c144827d259c3a03b5321
Dmitrii <dmkr0001@gmail.com>
2018-09-24 23:44:09 +0200

    very basic tuning of the std for the prior

---
+++ experiments-use-prior.sh
@@ -0,0 +1,31 @@
+# Room environment, same thing, but using a prior
+python test.py -e room -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 0.0625 -o results/use_prior
+python test.py -e room -p default -c use_prior -i sampling -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 0.0625 -o results/use_prior
+
+# Room environment, same thing, but using a prior
+python test.py -e room -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 0.125 -o results/use_prior
+python test.py -e room -p default -c use_prior -i sampling -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 0.125 -o results/use_prior
+
+# Room environment, same thing, but using a prior
+python test.py -e room -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 0.25 -o results/use_prior
+python test.py -e room -p default -c use_prior -i sampling -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 0.25 -o results/use_prior
+
+# Room environment, same thing, but using a prior
+python test.py -e room -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 0.5 -o results/use_prior
+python test.py -e room -p default -c use_prior -i sampling -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 0.5 -o results/use_prior
+
+# Room environment, same thing, but using a prior
+python test.py -e room -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 1 -o results/use_prior
+python test.py -e room -p default -c use_prior -i sampling -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 1 -o results/use_prior
+
+# Room environment, same thing, but using a prior
+python test.py -e room -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 2 -o results/use_prior
+python test.py -e room -p default -c use_prior -i sampling -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 2 -o results/use_prior
+
+# Room environment, same thing, but using a prior
+python test.py -e room -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 4 -o results/use_prior
+python test.py -e room -p default -c use_prior -i sampling -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 4 -o results/use_prior
+
+# Room environment, same thing, but using a prior
+python test.py -e room -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 8 -o results/use_prior
+python test.py -e room -p default -c use_prior -i sampling -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 8 -o results/use_prior
