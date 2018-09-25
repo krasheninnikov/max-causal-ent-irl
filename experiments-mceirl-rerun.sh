2655462b8f4391bc7c7878a07ca0238f7abbefe4
Dmitrii <dmkr0001@gmail.com>
2018-09-25 21:24:27 +0300

    deleted files

--- experiments-mceirl-rerun.sh
+++ experiments-mceirl-rerun.sh
@@ -4,88 +4,88 @@
 
 
 # ROOM mceirl
-python test.py -e room -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -w 0.0625 -o results/tuning
-python test.py -e room -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -w 0.125 -o results/tuning
-python test.py -e room -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -w 0.25 -o results/tuning
-python test.py -e room -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -w 0.5 -o results/tuning
-python test.py -e room -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -w 1 -o results/tuning
-python test.py -e room -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -w 2 -o results/tuning
-python test.py -e room -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -w 4 -o results/tuning
-python test.py -e room -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -w 8 -o results/tuning
+python test.py -e room -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -w 0.0625 -o results/tuning
+python test.py -e room -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -w 0.125 -o results/tuning
+python test.py -e room -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -w 0.25 -o results/tuning
+python test.py -e room -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -w 0.5 -o results/tuning
+python test.py -e room -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -w 1 -o results/tuning
+python test.py -e room -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -w 2 -o results/tuning
+python test.py -e room -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -w 4 -o results/tuning
+python test.py -e room -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -w 8 -o results/tuning
 
 # ROOM bad mceirl
-python test.py -e room -p bad -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -w 0.0625 -o results/tuning
-python test.py -e room -p bad -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -w 0.125 -o results/tuning
-python test.py -e room -p bad -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -w 0.25 -o results/tuning
-python test.py -e room -p bad -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -w 0.5 -o results/tuning
-python test.py -e room -p bad -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -w 1 -o results/tuning
-python test.py -e room -p bad -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -w 2 -o results/tuning
-python test.py -e room -p bad -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -w 4 -o results/tuning
-python test.py -e room -p bad -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -w 8 -o results/tuning
+python test.py -e room -p bad -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -w 0.0625 -o results/tuning
+python test.py -e room -p bad -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -w 0.125 -o results/tuning
+python test.py -e room -p bad -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -w 0.25 -o results/tuning
+python test.py -e room -p bad -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -w 0.5 -o results/tuning
+python test.py -e room -p bad -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -w 1 -o results/tuning
+python test.py -e room -p bad -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -w 2 -o results/tuning
+python test.py -e room -p bad -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -w 4 -o results/tuning
+python test.py -e room -p bad -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -w 8 -o results/tuning
 
 
 # Train mceirl
-python test.py -e train -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -w 0.0625 -o results/tuning
-python test.py -e train -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -w 0.125 -o results/tuning
-python test.py -e train -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -w 0.25 -o results/tuning
-python test.py -e train -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -w 0.5 -o results/tuning
-python test.py -e train -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -w 1 -o results/tuning
-python test.py -e train -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -w 2 -o results/tuning
-python test.py -e train -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -w 4 -o results/tuning
-python test.py -e train -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -w 8 -o results/tuning
+python test.py -e train -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -w 0.0625 -o results/tuning
+python test.py -e train -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -w 0.125 -o results/tuning
+python test.py -e train -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -w 0.25 -o results/tuning
+python test.py -e train -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -w 0.5 -o results/tuning
+python test.py -e train -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -w 1 -o results/tuning
+python test.py -e train -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -w 2 -o results/tuning
+python test.py -e train -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -w 4 -o results/tuning
+python test.py -e train -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -w 8 -o results/tuning
 
 
 # Apples mceirl
-python test.py -e apples -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -w 0.0625 -o results/tuning
-python test.py -e apples -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -w 0.125 -o results/tuning
-python test.py -e apples -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -w 0.25 -o results/tuning
-python test.py -e apples -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -w 0.5 -o results/tuning
-python test.py -e apples -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -w 1 -o results/tuning
-python test.py -e apples -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -w 2 -o results/tuning
-python test.py -e apples -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -w 4 -o results/tuning
-python test.py -e apples -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -w 8 -o results/tuning
+python test.py -e apples -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -w 0.0625 -o results/tuning
+python test.py -e apples -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -w 0.125 -o results/tuning
+python test.py -e apples -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -w 0.25 -o results/tuning
+python test.py -e apples -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -w 0.5 -o results/tuning
+python test.py -e apples -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -w 1 -o results/tuning
+python test.py -e apples -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -w 2 -o results/tuning
+python test.py -e apples -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -w 4 -o results/tuning
+python test.py -e apples -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -w 8 -o results/tuning
 
 ################################################################################
 ##########################            PRIOR, test k      #######################
 ################################################################################
 # ROOM mceirl
-python test.py -e room -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 0.0625 -o results/tuning
-python test.py -e room -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 0.125 -o results/tuning
-python test.py -e room -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 0.25 -o results/tuning
-python test.py -e room -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 0.5 -o results/tuning
-python test.py -e room -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 1 -o results/tuning
-python test.py -e room -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 2 -o results/tuning
-python test.py -e room -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 4 -o results/tuning
-python test.py -e room -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 8 -o results/tuning
+python test.py -e room -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -k 0.0625 -o results/tuning
+python test.py -e room -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -k 0.125 -o results/tuning
+python test.py -e room -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -k 0.25 -o results/tuning
+python test.py -e room -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -k 0.5 -o results/tuning
+python test.py -e room -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -k 1 -o results/tuning
+python test.py -e room -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -k 2 -o results/tuning
+python test.py -e room -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -k 4 -o results/tuning
+python test.py -e room -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -k 8 -o results/tuning
 
 # ROOM bad mceirl
-python test.py -e room -p bad -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 0.0625 -o results/tuning
-python test.py -e room -p bad -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 0.125 -o results/tuning
-python test.py -e room -p bad -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 0.25 -o results/tuning
-python test.py -e room -p bad -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 0.5 -o results/tuning
-python test.py -e room -p bad -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 1 -o results/tuning
-python test.py -e room -p bad -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 2 -o results/tuning
-python test.py -e room -p bad -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 4 -o results/tuning
-python test.py -e room -p bad -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 8 -o results/tuning
+python test.py -e room -p bad -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -k 0.0625 -o results/tuning
+python test.py -e room -p bad -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -k 0.125 -o results/tuning
+python test.py -e room -p bad -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -k 0.25 -o results/tuning
+python test.py -e room -p bad -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -k 0.5 -o results/tuning
+python test.py -e room -p bad -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -k 1 -o results/tuning
+python test.py -e room -p bad -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -k 2 -o results/tuning
+python test.py -e room -p bad -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -k 4 -o results/tuning
+python test.py -e room -p bad -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -k 8 -o results/tuning
 
 
 # TRAIN mceirl
-python test.py -e train -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 0.0625 -o results/tuning
-python test.py -e train -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 0.125 -o results/tuning
-python test.py -e train -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 0.25 -o results/tuning
-python test.py -e train -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 0.5 -o results/tuning
-python test.py -e train -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 1 -o results/tuning
-python test.py -e train -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 2 -o results/tuning
-python test.py -e train -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 4 -o results/tuning
-python test.py -e train -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 8 -o results/tuning
+python test.py -e train -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -k 0.0625 -o results/tuning
+python test.py -e train -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -k 0.125 -o results/tuning
+python test.py -e train -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -k 0.25 -o results/tuning
+python test.py -e train -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -k 0.5 -o results/tuning
+python test.py -e train -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -k 1 -o results/tuning
+python test.py -e train -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -k 2 -o results/tuning
+python test.py -e train -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -k 4 -o results/tuning
+python test.py -e train -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -k 8 -o results/tuning
 
 
 # Apples mceirl
-python test.py -e apples -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 0.0625 -o results/tuning
-python test.py -e apples -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 0.125 -o results/tuning
-python test.py -e apples -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 0.25 -o results/tuning
-python test.py -e apples -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 0.5 -o results/tuning
-python test.py -e apples -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 1 -o results/tuning
-python test.py -e apples -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 2 -o results/tuning
-python test.py -e apples -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 4 -o results/tuning
-python test.py -e apples -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 8 -o results/tuning
+python test.py -e apples -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -k 0.0625 -o results/tuning
+python test.py -e apples -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -k 0.125 -o results/tuning
+python test.py -e apples -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -k 0.25 -o results/tuning
+python test.py -e apples -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -k 0.5 -o results/tuning
+python test.py -e apples -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -k 1 -o results/tuning
+python test.py -e apples -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -k 2 -o results/tuning
+python test.py -e apples -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -k 4 -o results/tuning
+python test.py -e apples -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.001 -m 500 -H 10 -k 8 -o results/tuning
--- experiments-use-prior.sh
+++ experiments-use-prior.sh
@@ -1,31 +1,76 @@
-# Room environment, same thing, but using a prior
+# ROOM default
 python test.py -e room -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 0.0625 -o results/use_prior
 python test.py -e room -p default -c use_prior -i sampling -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 0.0625 -o results/use_prior
 
-# Room environment, same thing, but using a prior
 python test.py -e room -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 0.125 -o results/use_prior
 python test.py -e room -p default -c use_prior -i sampling -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 0.125 -o results/use_prior
 
-# Room environment, same thing, but using a prior
 python test.py -e room -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 0.25 -o results/use_prior
 python test.py -e room -p default -c use_prior -i sampling -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 0.25 -o results/use_prior
 
-# Room environment, same thing, but using a prior
 python test.py -e room -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 0.5 -o results/use_prior
 python test.py -e room -p default -c use_prior -i sampling -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 0.5 -o results/use_prior
 
-# Room environment, same thing, but using a prior
 python test.py -e room -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 1 -o results/use_prior
 python test.py -e room -p default -c use_prior -i sampling -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 1 -o results/use_prior
 
-# Room environment, same thing, but using a prior
 python test.py -e room -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 2 -o results/use_prior
 python test.py -e room -p default -c use_prior -i sampling -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 2 -o results/use_prior
 
-# Room environment, same thing, but using a prior
 python test.py -e room -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 4 -o results/use_prior
 python test.py -e room -p default -c use_prior -i sampling -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 4 -o results/use_prior
 
-# Room environment, same thing, but using a prior
 python test.py -e room -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 8 -o results/use_prior
 python test.py -e room -p default -c use_prior -i sampling -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 8 -o results/use_prior
+
+
+# ROOM bad
+python test.py -e room -p bad -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 0.0625 -o results/use_prior
+python test.py -e room -p bad -c use_prior -i sampling -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 0.0625 -o results/use_prior
+
+python test.py -e room -p bad -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 0.125 -o results/use_prior
+python test.py -e room -p bad -c use_prior -i sampling -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 0.125 -o results/use_prior
+
+python test.py -e room -p bad -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 0.25 -o results/use_prior
+python test.py -e room -p bad -c use_prior -i sampling -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 0.25 -o results/use_prior
+
+python test.py -e room -p bad -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 0.5 -o results/use_prior
+python test.py -e room -p bad -c use_prior -i sampling -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 0.5 -o results/use_prior
+
+python test.py -e room -p bad -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 1 -o results/use_prior
+python test.py -e room -p bad -c use_prior -i sampling -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 1 -o results/use_prior
+
+python test.py -e room -p bad -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 2 -o results/use_prior
+python test.py -e room -p bad -c use_prior -i sampling -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 2 -o results/use_prior
+
+python test.py -e room -p bad -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 4 -o results/use_prior
+python test.py -e room -p bad -c use_prior -i sampling -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 4 -o results/use_prior
+
+python test.py -e room -p bad -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 8 -o results/use_prior
+python test.py -e room -p bad -c use_prior -i sampling -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 8 -o results/use_prior
+
+
+# TRAIN default
+python test.py -e train -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 0.0625 -o results/use_prior
+python test.py -e train -p default -c use_prior -i sampling -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 0.0625 -o results/use_prior
+
+python test.py -e train -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 0.125 -o results/use_prior
+python test.py -e train -p default -c use_prior -i sampling -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 0.125 -o results/use_prior
+
+python test.py -e train -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 0.25 -o results/use_prior
+python test.py -e train -p default -c use_prior -i sampling -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 0.25 -o results/use_prior
+
+python test.py -e train -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 0.5 -o results/use_prior
+python test.py -e train -p default -c use_prior -i sampling -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 0.5 -o results/use_prior
+
+python test.py -e train -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 1 -o results/use_prior
+python test.py -e train -p default -c use_prior -i sampling -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 1 -o results/use_prior
+
+python test.py -e train -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 2 -o results/use_prior
+python test.py -e train -p default -c use_prior -i sampling -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 2 -o results/use_prior
+
+python test.py -e train -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 4 -o results/use_prior
+python test.py -e train -p default -c use_prior -i sampling -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 4 -o results/use_prior
+
+python test.py -e train -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 8 -o results/use_prior
+python test.py -e train -p default -c use_prior -i sampling -d true_reward,final_reward -s 0,1,2,3,4 -H 10 -k 8 -o results/use_prior
