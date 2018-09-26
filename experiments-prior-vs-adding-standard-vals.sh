7c4de5e4faab5c92debe0d8234b567a102e5f437
Dmitrii <dmkr0001@gmail.com>
2018-09-27 01:05:11 +0300

    a plot for the prior vs addition experiment

---
+++ experiments-prior-vs-adding-standard-vals.sh
@@ -0,0 +1,48 @@
+# ADD REWARDS
+python test.py -e room -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.003 -m 500  -H 10 -k 0.05 -o results/use_prior_vs_add_r
+python test.py -e room -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.003 -m 500  -H 10 -k 0.1 -o results/use_prior_vs_add_r
+python test.py -e room -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.003 -m 500  -H 10 -k 0.2 -o results/use_prior_vs_add_r
+python test.py -e room -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.003 -m 500  -H 10 -k 0.3 -o results/use_prior_vs_add_r
+python test.py -e room -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.003 -m 500  -H 10 -k 0.5 -o results/use_prior_vs_add_r
+python test.py -e room -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.003 -m 500  -H 10 -k 1 -o results/use_prior_vs_add_r
+python test.py -e room -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.003 -m 500  -H 10 -k 2 -o results/use_prior_vs_add_r
+python test.py -e room -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.003 -m 500  -H 10 -k 3 -o results/use_prior_vs_add_r
+python test.py -e room -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.003 -m 500  -H 10 -k 5 -o results/use_prior_vs_add_r
+python test.py -e room -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.003 -m 500  -H 10 -k 10 -o results/use_prior_vs_add_r
+
+
+python test.py -e train -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.003 -m 500  -H 8 -k 0.05 -o results/use_prior_vs_add_r
+python test.py -e train -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.003 -m 500  -H 8 -k 0.1 -o results/use_prior_vs_add_r
+python test.py -e train -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.003 -m 500  -H 8 -k 0.2 -o results/use_prior_vs_add_r
+python test.py -e train -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.003 -m 500  -H 8 -k 0.3 -o results/use_prior_vs_add_r
+python test.py -e train -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.003 -m 500  -H 8 -k 0.5 -o results/use_prior_vs_add_r
+python test.py -e train -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.003 -m 500  -H 8 -k 1 -o results/use_prior_vs_add_r
+python test.py -e train -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.003 -m 500  -H 8 -k 2 -o results/use_prior_vs_add_r
+python test.py -e train -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.003 -m 500  -H 8 -k 3 -o results/use_prior_vs_add_r
+python test.py -e train -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.003 -m 500  -H 8 -k 5 -o results/use_prior_vs_add_r
+python test.py -e train -p default -c add_rewards -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.003 -m 500  -H 8 -k 10 -o results/use_prior_vs_add_r
+
+
+# USE PRIOR
+python test.py -e room -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.003 -m 500  -H 10 -k 0.05 -o results/use_prior_vs_add_r
+python test.py -e room -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.003 -m 500  -H 10 -k 0.1 -o results/use_prior_vs_add_r
+python test.py -e room -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.003 -m 500  -H 10 -k 0.2 -o results/use_prior_vs_add_r
+python test.py -e room -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.003 -m 500  -H 10 -k 0.3 -o results/use_prior_vs_add_r
+python test.py -e room -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.003 -m 500  -H 10 -k 0.5 -o results/use_prior_vs_add_r
+python test.py -e room -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.003 -m 500  -H 10 -k 1 -o results/use_prior_vs_add_r
+python test.py -e room -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.003 -m 500  -H 10 -k 2 -o results/use_prior_vs_add_r
+python test.py -e room -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.003 -m 500  -H 10 -k 3 -o results/use_prior_vs_add_r
+python test.py -e room -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.003 -m 500  -H 10 -k 5 -o results/use_prior_vs_add_r
+python test.py -e room -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.003 -m 500  -H 10 -k 10 -o results/use_prior_vs_add_r
+
+
+python test.py -e train -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.003 -m 500  -H 8 -k 0.05 -o results/use_prior_vs_add_r
+python test.py -e train -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.003 -m 500  -H 8 -k 0.1 -o results/use_prior_vs_add_r
+python test.py -e train -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.003 -m 500  -H 8 -k 0.2 -o results/use_prior_vs_add_r
+python test.py -e train -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.003 -m 500  -H 8 -k 0.3 -o results/use_prior_vs_add_r
+python test.py -e train -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.003 -m 500  -H 8 -k 0.5 -o results/use_prior_vs_add_r
+python test.py -e train -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.003 -m 500  -H 8 -k 1 -o results/use_prior_vs_add_r
+python test.py -e train -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.003 -m 500  -H 8 -k 2 -o results/use_prior_vs_add_r
+python test.py -e train -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.003 -m 500  -H 8 -k 3 -o results/use_prior_vs_add_r
+python test.py -e train -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.003 -m 500  -H 8 -k 5 -o results/use_prior_vs_add_r
+python test.py -e train -p default -c use_prior -i mceirl -d true_reward,final_reward -s 0,1,2,3,4 -l 0.003 -m 500  -H 8 -k 10 -o results/use_prior_vs_add_r
