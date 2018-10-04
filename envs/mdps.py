413be0de2356a17522aa5eaa4005f786124a708e
krasheninnikov <dmkr0001@gmail.com>
2018-10-04 18:20:21 +0200

    toy mdp experiments to figure out what's happening when we use a uniform prior on s_{-T}

--- envs/mdps.py
+++ envs/mdps.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.sparse import lil_matrix
 
 
 class MDP(object):
@@ -45,6 +46,22 @@ class MDP(object):
                     if s_prime in s_a_s:
                         T[s, a, s_prime] = s_a_s[s_prime]
         return T
+
+    def get_transition_matrix_2D(self):
+        '''Return a matrix with index S,A,S' -> P(S'|S,A)'''
+        T = np.zeros([self.nS * self.nA, self.nS])
+        s_a_pos_index=0
+        for s in range(self.nS):
+            for a in range(self.nA):
+                transitions = self.P[s][a]
+                s_a_s = {t[1]:t[0] for t in transitions}
+                for s_prime in range(self.nS):
+                    if s_prime in s_a_s.keys():
+                        T[s_a_pos_index, s_prime] = s_a_s[s_prime]
+                s_a_pos_index+=1
+        self.T_matrix = T
+        self.T_matrix_transpose = T.transpose()
+
 
     def reset(self):
         self.s = 0
@@ -97,12 +114,15 @@ class MDP_toy_irreversibility(MDP):
 
         self.nS = 4 # number of states
         self.nA = 2
+        self.init_state = 0
+        self.f_matrix = np.eye(self.nS)
+        self.num_features=self.nS
         self.P = {}
         self.P.update({0:{0:[(1.0, 1,-.1)], 1:[(1.0, 3, 1)]}})
         self.P.update({1:{0:[(1.0, 2, 1)], 1:[(1.0, 0, 0)]}})
         self.P.update({2:{0:[(1.0, 2, 1)], 1:[(1.0, 1, -.1)]}})
         self.P.update({3:{0:[(1.0, 3, 1)], 1:[(1.0, 3, 1)]}})
-        self.T = self.get_transition_matrix()
+        self.get_transition_matrix_2D()
         self.reset()
 
     def reset(self):
